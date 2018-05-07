69ab708a7924e18e5d2e2ffb8406d795c7c4454d
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-05-07 16:55:43 -0700

    Generate word magnet div's from an array. Attempt to make word magnets draggable.

--- magneticPoetryApp/app.js
+++ magneticPoetryApp/app.js
@@ -1,13 +1,21 @@
-var wordsArr1 = ["the", "I", "me", "code", "now", "white", "black", "exasperating", "try", "love", "command", "mindset", "growth", "iterate", "diversity", "mental", "models", "success", "embrace"];
+window.onload = function makeDraggableWords() {
 
-function createWords(wordsArray) {
-  for (let i=0; i<wordsArr1.length; i++) {
-    let word=document.createElement('div');
-    document.word.innerHTML = wordsArray[i];
-    word.style.width = (5 * wordsArray[i].length) + "px";
-    word.style.backgroundColor = "white";
-    word.style.border = "solid 1px black";
-    word.style.boxShadow = " 5px 5px 2px grey";
-    document.body.appendChild(word);
+  function createWords(wordsArray) {
+    for (let i=0; i<wordsArray.length; i++) {
+      let word=document.createElement('div');
+      word.className = "words";
+      word.innerHTML = wordsArray[i];
+      word.style.width = (5 * wordsArray[i].length) + "px";
+      word.style.backgroundColor = "white";
+      word.style.border = "solid 1px black";
+      word.style.boxShadow = " 5px 5px 2px grey";
+      document.body.appendChild(word);
+    }
   }
+
+  var wordsArr1 = ["the", "I", "me", "code", "now", "white", "black", "exasperating", "try", "love", "command", "mindset", "growth", "iterate", "diversity", "mental", "models", "success"];
+
+  createWords(wordsArr1);
+
+
 }
